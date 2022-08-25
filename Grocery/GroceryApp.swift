86fdc2cd6843cc44b2feaa3b4876fa0ffd7f545b
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-08-25 11:59:05 +0100

    Initial Commit

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Temiloluwa on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
